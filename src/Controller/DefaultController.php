c7748d5bc45ab041d37676df14509482538eb3fa
jgauthi <jgauthi@users.noreply.github.com>
2018-12-12 22:20:08 +0100

    First controller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * @Route("/")
+*/
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default_index")
+    */
+    public function index()
+    {
+        return new JsonResponse([
+            'action'    =>  'index',
+            'time'      =>  time(),
+        ]);
+    }
+}
