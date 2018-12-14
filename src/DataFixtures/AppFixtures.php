aa3635856f2f579b9cddcd90d7636467a04fda22
jgauthi <jgauthi@users.noreply.github.com>
2018-12-14 16:04:08 +0100

    Entity: BlogPost, Comment - add relations + update fixtures (use Faker library)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\Comment;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -11,10 +12,12 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class AppFixtures extends Fixture
 {
     private $passwordEncode;
+    private $faker;
 
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->passwordEncode = $passwordEncoder;
+        $this->faker = \Faker\Factory::create();
     }
 
     public function load(ObjectManager $manager): void
@@ -42,19 +45,16 @@ class AppFixtures extends Fixture
     public function loadBlogPost(ObjectManager $manager): void
     {
         $user = $this->getReference('user_admin');
-        $data = [
-            ['title' => 'My blog post 1', 'content' => 'Lorem Ipsu'],
-            ['title' => 'My blog post 2', 'content' => 'Dolor color...'],
-            ['title' => 'My blog post 3', 'content' => 'Consectetur adipiscing elit'],
-        ];
 
-        foreach ($data as ['title' => $title, 'content' => $content]) {
+        for($i = 0; $i < 100; $i++) {
             $post = new BlogPost();
-            $post->setTitle($title)
-                ->setSlug( preg_replace('#[^a-z0-9]{1,}#i', '-', strtolower($title)) )
+            $post->setTitle($this->faker->realText(30))
+                ->setSlug( $this->faker->slug )
                 ->setAuthor($user)
-                ->setContent($content)
-                ->setPublished(new \DateTime('now'));
+                ->setContent($this->faker->realText())
+                ->setPublished($this->faker->dateTimeThisYear);
+
+            $this->setReference("blog_post_$i", $post);
 
             $manager->persist($post);
         }
@@ -64,6 +64,21 @@ class AppFixtures extends Fixture
 
     public function loadComment(ObjectManager $manager): void
     {
+        $user = $this->getReference('user_admin');
 
+        for($i = 0; $i < 100; $i++) {
+            for($j = 0; $j < rand(1,10); $j++) {
+                $comment = new Comment();
+                $comment->setAuthor($user)
+                    ->setContent($this->faker->realText())
+                    ->setPublished($this->faker->dateTimeThisYear)
+                    ->setBlogPost($this->getReference("blog_post_$i"))
+                ;
+
+                $manager->persist($comment);
+            }
+        }
+
+        $manager->flush();
     }
 }
--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -43,6 +45,17 @@ class BlogPost
      * @ORM\Column(type="string", length=255, nullable=true)
     */
     private $slug;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="blogPost")
+     */
+    private $comments;
+
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -105,6 +118,37 @@ class BlogPost
     public function setAuthor(?User $author): self
     {
         $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setBlogPost($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getBlogPost() === $this) {
+                $comment->setBlogPost(null);
+            }
+        }
 
         return $this;
     }
