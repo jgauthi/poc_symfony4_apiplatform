eee7eb766f8316fdc278853281b905338b571378
jgauthi <jgauthi@users.noreply.github.com>
2018-12-14 18:54:07 +0100

    EventSubscriber: hashing password (new)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
 /**
  * @ApiResource(
  *     itemOperations={"get"},
- *     collectionOperations={"get"},
+ *     collectionOperations={"post"},
  *     normalizationContext={
  *          "groups"={"read"}
  *     }
