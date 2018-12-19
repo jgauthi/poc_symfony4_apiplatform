57262b6f7168f53bff34728dfcd953ca9248d108
jgauthi <jgauthi@users.noreply.github.com>
2018-12-19 22:01:54 +0100

    Api platform: Entities - add sorting result by date

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -12,6 +12,7 @@ use Vich\UploaderBundle\Mapping\Annotation as Vich;
  * @ORM\Entity()
  * @Vich\Uploadable()
  * @ApiResource(
+ *     attributes={"order"={"id": "ASC"}},
  *     collectionOperations={
  *          "get",
  *          "post"={
