a729d58b8d6f50fbf7e02e431a8a67235f267340
jgauthi <jgauthi@users.noreply.github.com>
2019-08-16 18:48:02 +0200

    Fix some symfony 4.3 deprecations

--- src/Command/BlogPostViewCommand.php
+++ src/Command/BlogPostViewCommand.php
@@ -4,9 +4,8 @@ namespace App\Command;
 use App\Entity\BlogPost;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\{InputArgument, InputInterface, InputOption};
+use Symfony\Component\Console\Input\{InputArgument, InputInterface};
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 
 class BlogPostViewCommand extends Command
 {
